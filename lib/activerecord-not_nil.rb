c1dcadc89cd935d9229834b062556aa3990ab296
Nicke van Oorschot <nvanoorschot@users.noreply.github.com>
2023-10-13 14:57:21 +0200

    Require relative (#4)

--- lib/activerecord-not_nil.rb
+++ lib/activerecord-not_nil.rb
@@ -1,7 +1,10 @@
+# rubocop:disable Naming/FileName
 # frozen_string_literal: true
 
 require 'active_record'
 require 'not_niller'
 
-require 'activerecord-not_nil/activerecord/connection_adapters/quoting'
-require 'activerecord-not_nil/arel/visitors/to_sql'
+require_relative 'activerecord-not_nil/activerecord/connection_adapters/quoting'
+require_relative 'activerecord-not_nil/arel/visitors/to_sql'
+
+# rubocop:enable Naming/FileName
